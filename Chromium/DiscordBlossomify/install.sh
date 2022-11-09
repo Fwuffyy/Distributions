f6cdd4386b2abf87bcbf5285b491dfaed301e373
ChezCoder <62308406+ChezCoder@users.noreply.github.com>
2022-11-08 21:57:27 -0500

    DiscordThemer v1.0.0

--- Chromium/DiscordBlossomify/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "\033[32mExtension is not released... install bookmarklet at \033[34mhttps://dist.fwuffy.tk/blossomify.html\033[0m";
---
+++ Chromium/DiscordThemer/install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "\033[32mExtension is not released... install bookmarklet at \033[34mhttps://dist.fwuffy.tk\033[32m in the 'bookmarklets' section\033[0m";
